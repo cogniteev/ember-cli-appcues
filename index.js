1499dfbab8848d974822e767e752300b8f0d8066
Anthony Pessy <anthony@cogniteev.com>
2018-10-16 14:41:09 +0200

    inject appcues script unless accountId is missing or lazyMode

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  contentFor(type, config) {
+    const { lazyLoad, accountId } = config['ember-cli-appcues'] || {};
+    if (type === 'body' && accountId && !lazyLoad) {
+      return `<script type="text/javascript" src="//fast.appcues.com/${accountId}.js"></script>`;
+    }
+  }
 };
